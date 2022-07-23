9e7e9dbb577fe8a004819e413aa050bd648c206b
romanromannes <romanromannes@gmail.com>
2022-07-23 20:56:31 -0300

    delete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StudentListComponent } from './components/student-list/student-list.component';
-import { StudentComponent } from './components/student/student.component';
 import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentListComponent,
-    StudentComponent,
     NavComponent
   ],
   imports: [
